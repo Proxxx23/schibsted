3418a5fd486ba81c3e3689864cd376b3606a386a
Lukasz Matusik <kontakt@piwolucja.pl>
2018-12-04 22:42:52 +0100

    Initial Laravel

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+/**
+ * Class ListController
+ * @package App\Http\Controllers
+ */
+final class ListController
+{
+    public function list()
+    {
+        echo 'list';
+    }
+}
